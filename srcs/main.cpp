6a35cc08e8c2b20df174bbe512002777b1dc5113
dcdnce <pforesti@student.42nice.fr>
2023-11-24 11:28:27 +0100

    🐛 32*32 texture bug

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,15 +1,3 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   main.cpp                                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: pforesti <pforesti@student.42.fr>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/03 11:52:37 by difool            #+#    #+#             */
-/*   Updated: 2023/07/02 15:09:00 by pforesti         ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "glad/glad.h"
 #include "pfm/pfm.hpp"
 #include <cmath>
@@ -20,8 +8,32 @@
 #include "Logger.hpp"
 #include "Shader.hpp"
 
-
-void	interpolate(unsigned char * buffer, int step);
+void	interpolate(unsigned char * buffer, int step)
+{
+	for (int y = 0 ; y < 256 ; y += step)
+	{
+		for (int x = 0 ; x < 256 ; x += step)
+		{
+			float a = buffer[(y<<8)+x];
+			float b = buffer[(y<<8)+((x+step)&255)];
+			float c = buffer[(((y+step)&255)<<8)+x]; 
+			float d = buffer[(((y+step)&255)<<8)+((x+step)&255)]; 
+			for (int i = 0 ; i < step ; i++)
+			{
+				for (int j = 0 ; j < step ; j++)
+				{
+					float xf = (float)j / (float)step;
+					float yf = (float)i / (float)step;
+					xf = xf*xf*(3.f-2.f*xf);
+					yf = yf*yf*(3.f-2.f*yf);
+					float nx0 = a*(1.f-xf)+b*xf;
+					float nx1 = c*(1.f-xf)+d*xf;
+					buffer[(((y+i)<<8))+(x+j)] = nx0*(1.f-yf)+nx1*yf;
+				}
+			}
+		}
+	}
+}
 
 int	main(void)
 {
@@ -35,9 +47,9 @@ int	main(void)
 	// Noise related
 	unsigned char tex[8][256*256];
 	// For every octave fill the texture and interpolate
-	for (size_t i = 0 ; i < 8 ; i++)
+	for (int i = 0 ; i < 8 ; i++)
 	{
-		for (size_t j = 0 ; j < 256*256 ; j++)
+		for (int j = 0 ; j < 256*256 ; j++)
 			tex[i][j] = rand()&255;
 		interpolate(tex[i], 1<<i);
 	}
@@ -53,7 +65,7 @@ int	main(void)
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, 32, 32, 0, GL_RED, GL_UNSIGNED_BYTE, tex[0]);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_R8, 256, 256, 0, GL_RED, GL_UNSIGNED_BYTE, tex[3]);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	glBindTexture(GL_TEXTURE_2D, 0);
 	glUniform1i(glGetUniformLocation(shader.program, "texture1"), 0);
@@ -121,27 +133,3 @@ int	main(void)
 	return (0);
 }
 
-void	interpolate(unsigned char * buffer, int step)
-{
-	for (size_t y = 0 ; y < 256 ; y += step)
-	{
-		for (size_t x = 0 ; x < 256 ; x += step)
-		{
-			float a = buffer[(y<<8)+x];
-			float b = buffer[(y<<8) + ((x+step)&255)];
-			float c = buffer[(((y+step)&255)<<8) + x];
-			float d = buffer[(((y+step)&255)<<8) + ((x+step)&255)];
-			for (size_t i = 0 ; i < step ; i++)
-			{
-				for (size_t j = 0 ; j < step ; j++)
-				{
-					float xf = (float)i / (float)step;
-					float yf = (float)j / (float)step;
-					xf = xf*xf*(3.f-2.f*xf);
-					yf = yf*yf*(3.f-2.f*yf);
-            		buffer[((y+j)<<8)+(x+i)] = a + xf*(b-a) + yf*(c-a) + xf*yf*(a-b-c+d);
-				}
-			}
-		}
-	}
-}
