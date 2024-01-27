3cf59dbf4090e1fab066dda879497ee22a07f2b7
dcdnce <pforesti@student.42nice.fr>
2024-01-27 19:05:42 +0100

    🎨 shader pow(2)

--- shaders/skydome.fs
+++ shaders/skydome.fs
@@ -8,6 +8,7 @@ uniform sampler2D texture1;
 void main()
 {
     float value = texture(texture1, fragTexCoord).r;
+	value = pow(value, 2.0);
     vec3 color = vec3(value, value, value);
 	gl_FragColor = vec4(1.0 - color.r, 1.0 - color.g, 1.0 - color.b, 1.0 - color.r);
 
